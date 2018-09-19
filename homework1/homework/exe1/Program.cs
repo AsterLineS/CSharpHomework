042a5fe4cfb3f681ca4e2a98cb5cc2c642ef7661
AsterLineS <43178977+AsterLineS@users.noreply.github.com>
2018-09-19 19:47:33 +0800

    Add files via upload
    因上次作业命名问题导致部分文件重复，请助教查阅最近的文件，第二次作业也因命名问题导致重复，请助教查阅最近文件

---
+++ homework1/homework/exe1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class AddNum
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "";
+        string s2 = "";
+        int a = 0;
+        int b = 0;
+        Console.Write("Please input int a:");
+        s1 = Console.ReadLine();
+        a = int.Parse(s1);
+        Console.Write("Please input int b:");
+        s2 = Console.ReadLine();
+        b= int.Parse(s2);
+        Console.WriteLine(a+"*"+b+"="+a*b);
+    }
+}
+
